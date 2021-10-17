4be63f1ee5c238cd02c9e2c98af9807e1dc74790
Shayan-Irfan <ishayan767@gmail.com>
2021-10-17 20:22:14 +0500

    buy tokens completed

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,8 +5,9 @@ module.exports = async function (deployer) {
     await deployer.deploy(Token);
     const token = await Token.deployed();
 
-    await deployer.deploy(EthSwap);
+    await deployer.deploy(EthSwap, token.address);
     const ethSwap = await EthSwap.deployed();
 
-    await token.transfer(ethSwap.address, '1000000000000000000000000')
+    // Transfer all tokens to EtSwap (1 million)
+    await token.transfer(ethSwap.address, '1000000000000000000000000');
 };
